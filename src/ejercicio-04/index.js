e7760968f4fb9d83fbfeb614f1502e31937887db
isabelpsanch <isabelparraehs@gmail.com>
2023-11-06 13:03:26 +0100

    Añadir Ejercicio 4

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+function Product(name, price, quantity) {
+    this.name = name
+    this.price = price
+    this.quantity = quantity
+}
+const Product1 = new Product('Laptop', 999.99, 5)
+const product2 = new Product('Phone', 599,99, 10)
+
+console.log(Product1)
+console.log(product2)
