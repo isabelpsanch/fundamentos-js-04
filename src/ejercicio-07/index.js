fdd52ae1f91841f026db8190c0e3f00f3cf5f71a
isabelpsanch <isabelparraehs@gmail.com>
2023-11-06 14:17:36 +0100

    Añadir Ejercicio 07

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,13 @@
 // Escribe tu código aquí:
+class Product {
+    constructor(name, price, quantity) {
+        this.name = name
+        this.price = price
+        this.quantity = quantity
+    }
+    get totalPrice() {
+        return this.price * this.quantity
+    }
+}
+const product = new Product('Shirt', 25.99, 3)
+console.log('Total Price:', product.totalPrice)
