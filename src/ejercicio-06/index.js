7b4c47b2efa0769d2602dafb6e7e67cae53e72e2
isabelpsanch <isabelparraehs@gmail.com>
2023-11-06 14:07:57 +0100

    Añadir Ejercicio 6

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+class Square extends Rectangle {
+    constructor(sideLength) {
+        super(sideLength, sideLength)
+    }
+}
+const square = new Square(7)
+console.log('Area del cuadrado:', square.calculateArea())
