9f0c4dbc87b098617f6ee6e6a4aff4531342b8f6
isabelpsanch <isabelparraehs@gmail.com>
2023-11-06 14:41:45 +0100

    Añadir Ejercicio 9

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,14 @@
 // Escribe tu código aquí:
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    introduce() {
+        console.log(`Hola, soy ${this.name} y tengo ${this.age} años.`)
+
+    }
+}
+const person = new Person('Alice', 28) 
+person.introduce()
