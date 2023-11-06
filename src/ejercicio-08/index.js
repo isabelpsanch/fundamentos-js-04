423614570b6b2f045c7bce94ec0fcd30ca88f42a
isabelpsanch <isabelparraehs@gmail.com>
2023-11-06 14:28:34 +0100

    Añadir Ejercicio 8

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,22 @@
 // Escribe tu código aquí:
+class Product {
+    constructor(name, price, quantily) {
+        this.name = name
+        this.price = price
+        this.quantily = quantily
+    }
+    get quantily() {
+        return this._quantily
+    }
+    set quantily(newQuantity) {
+        this._quantily = newQuantity
+    }
+    get totalPrice() {
+        return this.price * this._quantily
+    }
+}
+const product = new Product('Hat', 19.99, 2)
+console.log('Total Price: ', product.totalPrice)
+
+product.quantily = 5
+console.log('Updated Total Price: ', product.totalPrice)
